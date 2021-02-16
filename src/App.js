76caaf9d3d049682d4b65745198aec3757ab8fb2
Juan Calle <juan.calle2@nordstrom.com>
2021-02-16 00:02:33 -0500

    delete a task

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { isEmpty } from "lodash"
+import { isEmpty, size } from "lodash"
 import React, { useState } from "react"
 import shortid from "shortid"
 
@@ -22,6 +22,12 @@ const App = () => {
     setTask("")
   }
 
+  const handleDeleteTask = (idTask) => {
+    console.log(idTask)
+    const filteredTasks = tasks.filter(task => task.id !== idTask)
+    setTasks(filteredTasks)
+  }
+
   return (
     <div className="container mt-5">
       <h1>Tasks</h1>
@@ -29,19 +35,29 @@ const App = () => {
       <div className="row">
         <div className="col-8">
           <h4 className="text-center">Tasks list</h4>
-          <ul className="list-group">
-          {tasks.map((task) => (
+          {size(tasks) === 0 ? (
+            <li className="list-group-item text-center">without tasks</li>
+          ) : (
+            <ul className="list-group">
+            {tasks.map((task) => (
              <li className="list-group-item" key={task.id}>
               <span className="lead">{task.name}</span>
-              <button className="btn btn-danger btn-sm float-right mx-2">Delete</button>
-              <button className="btn btn-warning btn-sm float-right">Edit</button>
+              <button 
+                className="btn btn-danger btn-sm float-right mx-2"
+                onClick={() => handleDeleteTask(task.id)}
+              >
+                Delete
+              </button>
+              <button className="btn btn-warning btn-sm float-right">
+                Edit
+              </button>
             </li>
           ))
-           }
-          </ul>
+          }
+          </ul>)}
         </div>
         <div className="col-4">
-          <h4 className="text-center">Form</h4>
+          <h4 className="text-center">Add Task</h4>
           <form onSubmit={addTask}>
             <input 
               type="text"
